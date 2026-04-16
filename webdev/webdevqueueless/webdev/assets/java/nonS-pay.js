185d66f5125b4c8a7ce219b27b78fa0574becd69
JamesTown0411 <ty202501342@wmsu.edu.ph>
2026-04-16 22:03:47 +0800

    inedit ko mga files and inarrange ang files into folders and subfolders. I also added some new files for the landing page and payment system. I modified the login page and student landing page to reflect the changes in the file structure.

---
+++ webdev/webdevqueueless/webdev/assets/java/nonS-pay.js
@@ -0,0 +1,70 @@
+
+        function chooseService(purpose) {
+            selectedService = purpose;
+
+            for (let i = 1; i <= 7; i++) {
+                document.getElementById("confirmBox" + i).style.display = "block";
+            }
+
+            // Determine requirements
+            let requirements = "";
+            switch (purpose) {
+                case "Enrollment":
+                case "EAT":
+                case "NAT":
+                case "Interview":
+                    requirements = "CET Results";
+                    break;
+                case "Shifting":
+                case "Readmission":
+                case "Miscellaneous":
+                case "Certificate":
+                case "Diploma":
+                    requirements = "School ID or COR";
+                    break;
+                case "Tuition Fee":
+                case "TOR":
+                case "ID Request":
+                case "ID Replacement":
+                    requirements = "COR and Required Amount";
+                    break;
+                case "Other fees":
+                case "ID Request":
+                    requirements = "School ID";
+                    break;
+                case "Scholarship":
+                    requirements = "Form 138 or any grade report";
+                    break;
+                default:
+                    requirements = "Check office for Requirements";
+            }
+
+            // Update all 8 confirm boxes dynamically
+            for (let i = 1; i <= 8; i++) {
+                document.getElementById("selectedText" + i).innerHTML = `<strong>You selected:</strong> ${purpose}`;
+                document.getElementById("requirementText" + i).innerHTML = `<strong>Requirements:</strong> ${requirements}`;
+            }
+        }
+        function addQueue() {
+
+            let queue = JSON.parse(localStorage.getItem("queueList")) || [];
+
+            let qNum = "Q-" + Math.floor(Math.random() * 1000).toString().padStart(3, '0');
+
+            let newQueue = {
+                id: qNum,
+                name: "Non-student User",
+                purpose: selectedService,
+                status: "waiting",
+                time: new Date().toLocaleTimeString()
+
+
+            };
+
+            queue.push(newQueue);
+
+            localStorage.setItem("queueList", JSON.stringify(queue));
+
+
+            window.location.href = "nonS-quenum.html";
+        }
